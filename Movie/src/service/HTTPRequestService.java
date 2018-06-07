4920cbf0209745f316fbdbf4a409d42d2c1ac546
lja672 <lja672@gmail.com>
2018-06-07 15:40:37 +0800

    Generated Models

---
+++ Movie/src/service/HTTPRequestService.java
@@ -0,0 +1,40 @@
+package service;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+import com.alibaba.fastjson.JSONObject;
+
+public class HTTPRequestService {
+  
+  /**
+   * 发送Get请求，返回JSONObject
+   * @param url
+   * @return JSONObject
+   */
+  public JSONObject doGet(String url) {
+    String resultStr = "";
+    // BufferedReader in = null;
+    try {
+      URL realUrl = new URL(url);
+      // 打开和URL之间的连接
+      URLConnection connection = realUrl.openConnection();
+      // 建立实际的连接
+      connection.connect();
+      // 定义 BufferedReader输入流来读取URL的响应
+      BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+      String line;
+      while ((line = br.readLine()) != null) {
+        resultStr += line;
+      }
+      JSONObject result = JSONObject.parseObject(resultStr);
+      return result;
+    } catch (Exception e) {
+      System.out.println("发送GET请求出现异常！" + e);
+      e.printStackTrace();
+      return null;
+    }
+  }
+}
---
+++ Movie/src/service/MovieApiService.java
@@ -0,0 +1,12 @@
+package service;
+
+public class MovieApiService {
+
+  //豆瓣 API key
+  final String key_douban = "0b2bdeda43b5688921839c8ecb20399b";
+  
+  /**
+   * 获取今日上映电影列表及详情
+   */
+//  public 
+}
