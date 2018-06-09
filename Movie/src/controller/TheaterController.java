db85e955676f7a0292da1eaa988d87c4df1027d3
zzy <zzy@10.254.238.242>
2018-06-09 17:44:45 +0800

    获取座位

--- Movie/src/controller/TheaterController.java
+++ Movie/src/controller/TheaterController.java
@@ -49,20 +49,22 @@ public class TheaterController extends Controller{
 	//新增影片
 	public void addShowing() {
 		BaseResponse baseResponse = new BaseResponse();
-		String movie_id = this.getPara("movie_id");
-		String show_time = this.getPara("show_time");
-		String auditorium_id = this.getPara("auditorium_id");
-		String price = this.getPara("price");
+		String movies = this.getPara("movies");
 		
 		try {
-			if(StrKit.isBlank(movie_id) || StrKit.isBlank(show_time) || StrKit.isBlank(auditorium_id) || StrKit.isBlank(price)) {
+			if(StrKit.isBlank(movies)) {
 				baseResponse.setResult(ResultCodeEnum.MISS_PARA);
 			}else {
-				if(theaterService.addShowing(movie_id,show_time,auditorium_id,price)) {
-					baseResponse.setResult(ResultCodeEnum.SUCCESS);
-				}else {
-					baseResponse.setResult(ResultCodeEnum.FAILED);
+				String[] allMovies = movies.split(",");
+				for(int i=0; i<allMovies.length; i++) {
+					String[] info = allMovies[i].split("-");
+					String movie_id = info[0];
+					String show_time = info[1];
+					String auditorium_id = info[2];
+					String price = info[3];
+					theaterService.addShowing(movie_id, show_time, auditorium_id, price);
 				}
+				baseResponse.setResult(ResultCodeEnum.SUCCESS);
 			}
 		} catch (Exception e) {
 			// TODO: handle exception
@@ -94,18 +96,19 @@ public class TheaterController extends Controller{
 		this.renderJson(baseResponse);
 	}
 	
-	//根据城市及放映时间获取所有的影院及放映时间
+	//根据电影id、城市及放映时间获取所有的影院及放映时间
 	public void getTheaterByCityAndTime() {
 		BaseResponse baseResponse = new BaseResponse();
+		String movie_id = this.getPara("movie_id");
 		String city = this.getPara("city");
 		String time = this.getPara("time");
 		
 		try {
-			if(StrKit.isBlank(time) ||StrKit.isBlank(city)) {
+			if(StrKit.isBlank(time) ||StrKit.isBlank(city) || StrKit.isBlank(movie_id)) {
 				baseResponse.setResult(ResultCodeEnum.MISS_PARA);
 			}else {
-				//根据城市及放映时间获取所有的影院及放映时间
-				List<Showing> auditoriums = showingService.getAllAuditoriums(time,city);
+				//根据电影id、城市及放映时间获取所有的影院及放映时间
+				List<Showing> auditoriums = showingService.getAllAuditoriums(time,city,movie_id);
 				baseResponse.setData(auditoriums);
 				baseResponse.setResult(ResultCodeEnum.SUCCESS);
 			}
--- Movie/src/service/ShowingService.java
+++ Movie/src/service/ShowingService.java
@@ -7,7 +7,7 @@ import model.Showing;
 
 public class ShowingService {
 	//根据时间获取所有的放映厅及放映时间
-	public List<Showing> getAllAuditoriums(String time,String city) {
-		return Showing.dao.find("select theater.theater_id,show_time from showing,auditorium,theater where showing.auditorium_id = auditorium.auditorium_id and auditorium.theater_id = theater.theater_id and date_format(show_time,'%Y-%m-%d') = ? and city = ?",time,city); 
+	public List<Showing> getAllAuditoriums(String time,String city,String movie_id) {
+		return Showing.dao.find("select theater.theater_id,show_time,showing_id from showing,auditorium,theater where showing.auditorium_id = auditorium.auditorium_id and auditorium.theater_id = theater.theater_id and date_format(show_time,'%Y-%m-%d') = ? and movie_id = ? and city = ?",time,Integer.parseInt(movie_id),city); 
 	}
 }
