c30baadfde57b210f9905bfb3d90e633a9c8980e
Jiaan LIU <Jiaan LIU@Earth>
2018-06-16 10:26:34 +0800

    Added states of seat

--- Movie/src/service/SeatService.java
+++ Movie/src/service/SeatService.java
@@ -1,6 +1,9 @@
 package service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import model.Seat;
 
@@ -29,4 +32,54 @@ public class SeatService {
 	  Seat result = Seat.dao.findFirst("select * from seat,showing where seat.auditorium_id = showing.auditorium_id AND showing.showing_id=? AND seat.seat=?", showingId, seatName);
 	  return result;
 	}
+	
+	/**
+	 * 根据showing_id获取所有可用的seat
+	 * @param showingId
+	 * @return
+	 */
+	public List<Seat> availableSeats(int showingId){
+	  String sql = "select * from seat where seat.seat_id in ( select seat_id from booking where   booking.showing_id = ?)";
+	  List<Seat> seats = Seat.dao.find(sql, showingId);
+	  return seats;
+	}  
+	
+  /**
+   * 根据showing_id获取所有可用的seat
+   * @param showingId
+   * @return
+   */
+  public List<Seat> notAvailableSeats(int showingId){
+    String sql = "select * from seat where seat.seat_id not in ( select seat_id from booking where   booking.showing_id = ?)";
+    List<Seat> seats = Seat.dao.find(sql, showingId);
+    return seats;
+  }
+  
+  /**
+   * 获取所有位置，包含是否可用的信息
+   * available = 1 为不可用，available = 0 为可用
+   * @param showingId
+   * @return
+   */
+  public ArrayList<Seat> seatWithState(int showingId){
+    ArrayList<Seat> seats = new ArrayList<>();
+    List<Seat> available = this.availableSeats(showingId);
+    List<Seat> notAvailable = this.notAvailableSeats(showingId);
+    if(available != null && !available.isEmpty()) {
+      seats.addAll(available);
+      for (Seat seat : seats) {
+        seat.put("available", 0);
+      }
+    }
+    if(notAvailable != null && !notAvailable.isEmpty()) {
+      seats.addAll(notAvailable);
+      for (Seat seat : seats) {
+        seat.put("available", 1);
+      }
+    }
+    if(seats.isEmpty()) {
+      return null;
+    }
+    return seats;
+  }
 }
